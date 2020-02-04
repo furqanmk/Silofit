0d018741774e2c33f0a32a74b46b6aa9571d8071
Furqan Khan <furqanmk92@gmail.com>
2020-02-04 19:20:40 +0100

    Initial Commit

---
+++ SilofitTests/SilofitTests.swift
@@ -0,0 +1,34 @@
+//
+//  SilofitTests.swift
+//  SilofitTests
+//
+//  Created by Furqan Khan on 2/4/20.
+//  Copyright © 2020 Silofit. All rights reserved.
+//
+
+import XCTest
+@testable import Silofit
+
+class SilofitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
